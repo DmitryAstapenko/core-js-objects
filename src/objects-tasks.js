9bda3d791b63f5ecfcdd6d845c6ebdf62aca6307
DmitryAstapenko <dmitryastapenko1@gmail.com>
2025-12-05 19:31:58 +0300

    feat: complete some tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const foo = {};
+  return Object.assign(foo, { ...obj });
 }
 
 /**
@@ -32,8 +33,20 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const foo = {};
+
+  objects.forEach((obj) =>
+    Object.entries(obj).forEach(([key, value]) => {
+      if (foo[key] === undefined) {
+        foo[key] = value;
+      } else {
+        foo[key] += value;
+      }
+    })
+  );
+
+  return foo;
 }
 
 /**
@@ -49,8 +62,12 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const object = { ...obj };
+
+  keys.forEach((key) => delete object[key]);
+
+  return object;
 }
 
 /**
