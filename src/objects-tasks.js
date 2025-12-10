aadc89d83626b5bf65190c69b68620a55baccd2d
unknown <C:\Users\trydmi\AppData\Roaming\The Bat!>
2025-12-10 12:41:20 +0300

    feat: complete some tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -82,8 +82,18 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const objKeys1 = Object.keys(obj1);
+  const objKeys2 = Object.keys(obj2);
+  let result = true;
+
+  if (objKeys1.length !== objKeys2.length) return false;
+
+  objKeys1.forEach((key) => {
+    if (obj1[key] !== obj2[key]) result = false;
+  });
+
+  return result;
 }
 
 /**
@@ -97,8 +107,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length;
 }
 
 /**
@@ -117,8 +127,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -131,8 +141,16 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const word = [];
+
+  Object.keys(lettersObject).forEach((key) => {
+    lettersObject[key].forEach((position) => {
+      word[position] = key;
+    });
+  });
+
+  return word.join('');
 }
 
 /**
@@ -149,8 +167,34 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const cash = new Map();
+
+  for (let i = 0; i < queue.length; i += 1) {
+    const bill = queue[i];
+
+    if (bill === 25) {
+      cash.set(25, cash.has(25) ? cash.get(25) + 1 : 1);
+    } else if (bill === 50) {
+      if (cash.get(25) > 0) {
+        cash.set(25, cash.get(25) - 1);
+        cash.set(50, cash.has(50) ? cash.get(50) + 1 : 1);
+      } else {
+        return false;
+      }
+    } else if (bill === 100) {
+      if (cash.get(25) > 0 && cash.get(50) > 0) {
+        cash.set(25, cash.get(25) - 1);
+        cash.set(50, cash.get(50) - 1);
+      } else if (cash.get(25) >= 3) {
+        cash.set(25, cash.get(25) - 3);
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 /**
@@ -166,8 +210,14 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 /**
@@ -180,8 +230,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -195,8 +245,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 /**
@@ -230,8 +280,18 @@ function fromJSON(/* proto, json */) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr
+    .sort((a, b) => {
+      if (a.city === b.city) return 0;
+      if (a.city > b.city) return 1;
+      return -1;
+    })
+    .sort((a, b) => {
+      if (a.country === b.country) return 0;
+      if (a.country > b.country) return 1;
+      return -1;
+    });
 }
 
 /**
